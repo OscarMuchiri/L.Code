c731027ff771c2a34b39eec799f489d07339d4d0
OscarMuchiri <153835380+OscarMuchiri@users.noreply.github.com>
2025-05-06 20:00:41 +0300

    Time: 7 ms (81.22%), Space: 17.9 MB (95.25%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        L = 0  # Left pointer
+        R = n - 1  # Right pointer
+        
+        while L < R:
+            # Skip non-alphanumeric characters from the left
+            if not s[L].isalnum():
+                L += 1
+                continue  # Skip the rest of the loop and move to the next character
+            
+            # Skip non-alphanumeric characters from the right
+            if not s[R].isalnum():
+                R -= 1
+                continue  # Skip the rest of the loop and move to the next character
+            
+            # Compare the characters in lowercase
+            if s[L].lower() != s[R].lower():
+                return False  # If mismatch found, it's not a palindrome
+            
+            # Move both pointers toward the center
+            L += 1
+            R -= 1
+        
+        return True  # If all matching characters, it's a palindrome
+
+
+
+            
