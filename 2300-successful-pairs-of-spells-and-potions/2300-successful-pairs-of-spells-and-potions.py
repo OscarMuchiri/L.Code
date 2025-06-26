0f1fa6dfb7bc4d2d8ca008df28f1ec90bb2d29c2
OscarMuchiri <153835380+OscarMuchiri@users.noreply.github.com>
2025-06-26 23:37:12 +0300

    Update 2300-successful-pairs-of-spells-and-potions.py

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,21 +1,30 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        # Sort potions to allow binary search
         potions.sort()
         res = []
 
+        # Loop through each spell
         for s in spells:
+            # Binary search to find the first potion that makes a successful pair
             l, r = 0, len(potions) - 1
-            idx = len(potions)
+            idx = len(potions)  # Default to no successful pair
 
             while l <= r:
-                m = (l + r) // 2
-                
+                m = (l + r) // 2  # Middle index
+                # Check if the product of current spell and potion is >= success
                 if s * potions[m] >= success:
+                    # Move left to find earlier possible success
                     r = m - 1
-                    idx = m
+                    idx = m  # Update index of first successful potion
                 else:
-                    l = m + 1 
+                    # Move right to search for stronger potions
+                    l = m + 1
+
+            # The number of successful potions = total potions - index of first success
             res.append(len(potions) - idx)
+
         return res
+
 
         
