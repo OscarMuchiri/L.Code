1daf83561c4c9c95c8b0703708d901abca6be78d
OscarMuchiri <153835380+OscarMuchiri@users.noreply.github.com>
2025-07-19 21:23:04 +0300

    Update 0058-length-of-last-word.py

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -2,8 +2,8 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         #Easier to Start from the end of the statement
         #Skipping any trailing whitespaces at the end of the string
-        #Count the characters in the last word
-        #Then Return the length of the last word
+        #Then Count the characters in the last word
+        #Return the length of the last word
         i, length = len(s) - 1, 0
         while s[i] == ' ':
             i -= 1
